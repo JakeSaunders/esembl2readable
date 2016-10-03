2a75fdd1d46def24fc8c502bd3803ee1f1b04c84
Jake Saunders <jakesaunders@gmail.com>
2016-10-03 16:07:35 -0400

    Add files via upload
    fixed problem that would occur if ensembl ID wasn't in database

--- ensemblID2readable.R
+++ ensemblID2readable.R
@@ -11,12 +11,18 @@ ensemblID2readable <- function(ensembl.ids, species = "hsapiens_gene_ensembl"){
     # "drerio_gene_ensemb"
     # consider others from  listDatasets(mart =  useMart("ensembl"))
     
-    getBM(
+    readable <- getBM(
         attributes = c("ensembl_gene_id", "hgnc_symbol","description"),
         filters = "ensembl_gene_id",
         values = ensembl.ids,
         mart = ensembl
     )
+    
+    if (dim(readable)[1] == 0) {
+        cbind.data.frame("ensembl_gene_id" = ensembl.ids, "hgnc_symbol" = NA , "description" = "not in database")
+    }else{
+        readable
+    }
 }
 
 # http://may2009.archive.ensembl.org/biomart/martview
